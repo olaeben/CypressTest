8c061265807ad81d18bd32721f3fb2e953a53115
olaeben <ibitoyewale@gmail.com>
2022-12-19 10:50:48 +0100

    UpdateCart

--- cypress/e2e/cart.spec.js
+++ cypress/e2e/cart.spec.js
@@ -7,8 +7,8 @@ describe('Sauce Demo checkout process', () => {
       cy.get('#password').type('secret_sauce');
       cy.get('#login-button').click();
     });
+
     
-  
     it('should add items to the cart and checkout', () => {
       cy.get('#item_4_title_link').click();
       cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
